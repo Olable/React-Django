f3ac51b940241025a432a6e6cd0eddecbde81ae5
ola <olable9@gmail.com>
2025-11-19 02:14:59 -1100

    nc2

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Server(models.Model):
+    
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='owned_servers')
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='server_category')
+    description = models.CharField(null=True, max_length=255)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='servers', blank=True)  
+        
+        
+    def __str__(self):
+        return self.name 
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='channel_servers')
+    topic = models.CharField(max_length=100)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name='channels')
+    
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super(Channel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name 
--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets 
 
-# Create your views here.
+class ServerListViewSet(viewsets.ViewSet):
+
+    queryset = Server.objects.all()
+    serializer_class = ServerSerializer
+    def list(self, request):
+        category = request.query_params.get('category', None)   
+        return render(request, 'server_list.html')
+
