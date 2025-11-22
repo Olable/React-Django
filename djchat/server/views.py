29c8196418cfdef7755a788fefb185d45d67175b
ola <olable9@gmail.com>
2025-11-22 05:45:21 -1100

    nc3

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets 
 
-class ServerListViewSet(viewsets.ViewSet):
+#class ServerListViewSet(viewsets.ViewSet):
 
-    queryset = Server.objects.all()
-    serializer_class = ServerSerializer
-    def list(self, request):
-        category = request.query_params.get('category', None)   
-        return render(request, 'server_list.html')
+    #queryset = Server.objects.all()
+    #serializer_class = ServerSerializer
+    #def list(self, request):
+        #category = request.query_params.get('category', None)   
+        #return render(request, 'server_list.html')
 
