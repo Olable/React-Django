37d934ca0b70ec10c6e69de4418f6b7dfa6e5daa
olable <olable9@gmail.com>
2026-08-17 00:44:59 +0100

    fix: correct category filter lookup and router URL prefix

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
-from rest_framework import viewsets 
+from rest_framework import viewsets
 from rest_framework.response import Response
+
 from .models import Server
 from .serializer import ServerSerializer
 
+
 class ServerListViewSet(viewsets.ViewSet):
+    queryset = Server.objects.all()
 
-   queryset = Server.objects.all()
+    def list(self, request):
+        queryset = Server.objects.all()
 
-   def list(self, request):
-      category = request.query_params.get('category')
+        category = request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__name=category)
 
-      if category:
-         self.queryset.filter(category=category)  
-
-      serializer = ServerSerializer(self.queryset, many=True)
-      return Response(serializer.data)
+        serializer = ServerSerializer(queryset, many=True)
+        return Response(serializer.data)
